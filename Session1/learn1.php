0c428984c6400bcd32da83c127e6899a85536d7e
Dang Vinh <tranhgio15@gmail.com>
2019-09-30 20:14:43 +0700

    Bai 1

--- Session1/learn1.php
+++ Session1/learn1.php
@@ -1,3 +1,35 @@
 <?php
  echo "Hello world"
 ?>
+<?php
+    //Bien trong PHP
+    $myClass = "WAD_PHP\n";
+    echo $myClass;
+
+    $lopToi = "WAD_PHP2\n";
+    echo $lopToi;
+
+    $so1 = 5;
+    $so2 = 10;
+    echo "<br/>";
+    echo $so1 + $so2;
+    //Ham trong php
+    function myFunction(){
+        echo "Test function";
+    }
+    echo "</br>";
+    myFunction();
+
+    function myFunction1(){
+        $a = 10;
+        $b = 8;
+        return $a + $b;
+    }
+    echo myFunction1();
+    echo "<br/>";
+    function myFunction2($a = 5, $b =12){
+        return $a + $b;
+    }
+    echo "<br/>";
+    echo myFunction2(10,57);
+?>
