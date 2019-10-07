da2aef1b9928b00b16d527fd791bf83074c01a9c
Vinh <vinh.nguyen@asiantech.vn>
2019-10-07 18:20:36 +0700

    Commit session 2 excercise

---
+++ Session2/form.php
@@ -0,0 +1,51 @@
+<?php
+    if(isset($_POST['register'])){
+        if($_POST['username']!="" && $_POST['useremail']!="" && $_POST['userpassword']!=""){
+            echo $_POST['username'];
+            echo "</br>";
+            echo $_POST['useremail'];
+            echo "</br>";
+            echo $_POST['userpassword'];
+        }
+        
+    }
+?>
+
+<h1> Form Example </h1>
+
+<form name="registerForm" action="form.php" method="POST">
+    <p> Name:
+        <input type="text" name="username">
+        <?php
+        if(isset($_POST['register'])){
+            if($_POST['username']==""){
+                echo "<span style='color:red'>User Name require!!!</span>";
+            }
+        }
+        ?>
+    </p>
+    <p> Email:
+        <input type="text" name="useremail">
+        <?php
+        if(isset($_POST['register'])){
+            if(strlen($_POST['useremail'])==0){
+                echo "<span style='color:red'>Email require!!!</span>";
+            }
+        }
+        ?>
+    </p>
+    <p> Password:
+        <input type="password" name="userpassword">
+        <?php
+        if(isset($_POST['register'])){
+            if($_POST['userpassword']==""){
+                echo "<span style='color:red'>Password require!!!</span>";
+            }
+        }
+        ?>
+    </p>
+    
+    <p>
+        <input type="submit" name="register" value="Register">
+    </p>
+</form>
